b41f2a3bdb4d826ffbc1d94df892105e39a44483
Joe-Bills <josephbilla746@gmail.com>
2025-09-05 07:06:38 +0300

    Fix production settings for Railway deployment

--- moto_spares_manager/settings_production.py
+++ moto_spares_manager/settings_production.py
@@ -3,38 +3,24 @@ Production settings for moto_spares_manager project.
 """
 
 from .settings import *
-from decouple import config
-
-# Try to import dj_database_url, fallback if not available
-try:
-    import dj_database_url
-except ImportError:
-    dj_database_url = None
+import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-qx_pluhm1zno!$2q%j@hi#5haj+lx0^$r%_0g3iap&see#ureh')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-qx_pluhm1zno!$2q%j@hi#5haj+lx0^$r%_0g3iap&see#ureh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-if dj_database_url:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=config('DATABASE_URL', default='sqlite:///db.sqlite3')
-        )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    # Fallback to SQLite if dj_database_url is not available
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
@@ -52,16 +38,16 @@ X_FRAME_OPTIONS = 'DENY'
 
 # CORS settings for production
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='https://web-production-3b1a6.up.railway.app').split(',')
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'https://web-production-22b33.up.railway.app').split(',')
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 # Logging
 LOGGING = {
